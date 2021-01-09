328c08e3d24bf25451ce3c4d6f3c3fc722fbc9a6
Vadim Raksha <vadim.raksha@restream.io>
2021-01-09 21:28:00 +0200

    Update persistence.py

--- persistence.py
+++ persistence.py
@@ -3,50 +3,56 @@ import json
 module_name_users = 'users'
 module_name_updates = 'updates'
 
-class Persistence:
+def loadFile(moduleName, defaultValue):
+    try:
+        with open("{0}.json".format(moduleName)) as file:
+            return json.load(file)
+    except FileNotFoundError:
+        # Initialize for first start
+        return defaultValue
+
+def saveFile(jsonValue, moduleName):
+    with open("{0}.json".format(moduleName), 'w') as outfile:
+        json.dump(jsonValue, outfile)
+
+
+class UsersDatabase:
     def __init__(self):
-        self.users = self.loadFile(module_name_users, [])
-        self.updates = self.loadFile(module_name_updates, {"id":0})
-            
-    def loadFile(self, moduleName, defaultValue):
-        try:
-            with open("{0}.json".format(moduleName)) as file:
-                return json.load(file)
-        except FileNotFoundError:
-            # Initialize for first start
-            return defaultValue
-    
+        self.users = loadFile(module_name_users, [])
+
     def registerUser(self, id):
         self.users.append(id)
-        self.save_users()
+        self.save()
 
     def unregisterUser(self, id):
         self.users.remove(id)
-        self.save_users()
+        self.save()
 
     def isRegisteredUser(self, id):
         return id in self.users
 
     def allUsers(self):
-        return self.users
-    
+        return self.users    
+
+    def save(self):
+        saveFile(self.users, module_name_users)
+
+
+class UpdatesDatabase:
+    def __init__(self):
+        self.updates = loadFile(module_name_updates, {"id":0})
+
     def registerLastUpdate(self, id):
         self.updates = {"id":id}
-        self.save_updates()
-    
+        self.save()
+
     def getLastUpdate(self):
         try:
             return self.updates["id"]
         except Exception:
             return 0
 
-    def save_users(self):
-        self.save(self.users, module_name_users)
-     
-    def save_updates(self):
-        self.save(self.updates, module_name_updates)
-    
-    def save(self, jsonValue, moduleName):
-        with open("{0}.json".format(moduleName), 'w') as outfile:
-            json.dump(jsonValue, outfile)
+    def save(self):
+        saveFile(self.updates, module_name_updates)
 
+        
