bd3821a6a860eac9102d0f8339a1753dd0ee2c9f
Vadim Raksha <vadim.raksha@restream.io>
2021-01-09 21:13:35 +0200

    Update persistence.py

--- persistence.py
+++ persistence.py
@@ -1,13 +1,24 @@
 import json
 
+module_name_users = 'users'
+module_name_updates = 'updates'
+
 class Persistence:
     def __init__(self):
+        self.loadFile(module_name_users, [])
+        self.loadFile(module_name_updates, {id:0})
+            
+    def loadFile(self, moduleName, defaultValue):
         try:
-            with open('users.json') as file:
-                self.users = json.load(file)
+            with open("{0}.json".format(moduleName)) as file:
+                self[moduleName] = json.load(file)
         except FileNotFoundError:
             # Initialize for first start
-            self.users = []
+            self[moduleName] = defaultValue
+    
+    def saveFlie(self, moduleName):
+        with open("{0}.json".format(moduleName), 'w') as outfile:
+            json.dump(self[moduleName], outfile)
 
     def registerUser(self, id):
         self.users.append(id)
@@ -22,8 +33,12 @@ class Persistence:
 
     def allUsers(self):
         return self.users
+    
+    def registerLastUpdate(self, id):
+        self.updates.id = id
+        self.save()
 
     def save(self):
-        with open('users.json', 'w') as outfile:
-            json.dump(self.users, outfile)
+        saveFlie(module_name_users)
+        saveFlie(module_name_updates)
 
